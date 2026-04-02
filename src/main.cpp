7c611286172ef93a995bce0371507eceba1a3c8d
HussainDzaki <163353530+HussainDzaki@users.noreply.github.com>
2026-04-02 19:57:00 +0700

    move : testing file from data to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,50 @@
-#include <iostream>
-#include <fstream>
-#include "lexer/Lexer.hpp"
 #include "common/Error.hpp"
 #include "common/Reader.hpp"
 #include "common/Writer.hpp"
+#include "lexer/Lexer.hpp"
+#include <fstream>
+#include <iostream>
 
 int main()
 {
-    std::string inputPath, outputPath;
+    while (true) {
+        /* code */
 
-    std::cout << "Source code file path: ";
-    std::cin >> inputPath;
+        std::string inputPath, outputPath;
 
-    std::cout << "Output file path: ";
-    std::cin >> outputPath;
+        std::cout << "Source code file path: ";
+        std::cin >> inputPath;
 
-    // std::string sourceCode = "program Hallo\n";
-    try
-    {
-        std::string sourceCode = readFile(inputPath);
-        LexicalAnalyzer lexer(sourceCode);
-        std::vector<Token> tokens = lexer.tokenizeAll();
-        const std::vector<Token> &errorTokens = lexer.getErrorTokens();
-        // for (auto &&token : tokens)
-        // {
-        //     std::cout << token.value << ": "<< tokenTypeToString(token.type) << "\n";
-        // }
+        std::cout << "Output file path: ";
+        std::cin >> outputPath;
 
-        writeOutput(tokens, outputPath);
+        // std::string sourceCode = "program Hallo\n";
+        try {
+            std::string sourceCode = readFile(inputPath);
+            LexicalAnalyzer lexer(sourceCode);
+            std::vector<Token> tokens = lexer.tokenizeAll();
+            const std::vector<Token> &errorTokens = lexer.getErrorTokens();
+            // for (auto &&token : tokens)
+            // {
+            //     std::cout << token.value << ": "<< tokenTypeToString(token.type) << "\n";
+            // }
 
-        if (!errorTokens.empty())
-        {
-            std::cout << "\nLexical errors found:\n";
-            for (const auto &errorToken : errorTokens)
-            {
-                std::cout << "- Line " << errorToken.line << ", Col " << errorToken.column << ": " << errorTypeToMessage(errorToken.errorType, errorToken.value) << "\n";
+            writeOutput(tokens, outputPath);
+
+            if (!errorTokens.empty()) {
+                std::cout << "\nLexical errors found:\n";
+                for (const auto &errorToken : errorTokens) {
+                    std::cout << "- Line " << errorToken.line << ", Col " << errorToken.column << ": " << errorTypeToMessage(errorToken.errorType, errorToken.value) << "\n";
+                }
+                return 0;
             }
+
+            std::cout << "berhasil kah? \n";
             return 0;
         }
-
-        std::cout << "berhasil kah? \n";
-        return 0;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Terjadi Kesalahan Sistem: " << e.what() << "\n";
-        return 1;
+        catch (const std::exception &e) {
+            std::cerr << "Terjadi Kesalahan Sistem: " << e.what() << "\n";
+            return 1;
+        }
     }
 }
